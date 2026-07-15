d53535155419567a057f1519321a4ac0c7f962c0
Shubham <shubham86.y@gmail.com>
2026-07-15 21:32:45 +0530

    Initial commit

--- largestelement.cpp
+++ largestelement.cpp
@@ -8,5 +8,12 @@ int main(){
     for(int i=0;i<5;i++){
         cin >> arr[i];
     }
-    
+    int max=arr[0];
+    for(int i=0;i<5;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout << "largest element is: " << max << endl;
+    return 0;
 }
