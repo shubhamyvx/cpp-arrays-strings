8566c63aa7b0dab1121ae62b8d738aa370107805
Shubham <shubham86.y@gmail.com>
2026-07-15 21:44:15 +0530

    Initial commit

--- Sumofall.cpp
+++ Sumofall.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <array>
+using namespace std;
 
+int main(){
+    int sum = 0;
+    array<int,5> arr;
+    cout << "Enter elements :";
+    for(int i = 0; i<5 ; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0 ; i<5 ; i++){
+        sum = sum + arr[i];
+    }
+    cout << "Sum of all elements :" << sum << endl;
+    return 0;
+}
