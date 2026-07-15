7be1d611956947d12b1bb3efc08124d087a2a24d
Shubham <shubham86.y@gmail.com>
2026-07-15 21:21:13 +0530

    Initial commit fixed

---
+++ largestelement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main(){
+    array<int,5> arr;
+    cout << "Enter elements:";
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+    }
+    
+}
