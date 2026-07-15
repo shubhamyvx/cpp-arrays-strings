be7670fe1429c965744d74e25be18731fc0ba298
Shubham <shubham86.y@gmail.com>
2026-07-15 21:35:27 +0530

    Initial commit

--- stdlibraryarray.cpp
+++ stdlibraryarray.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main(){
+    array<int,5> arr={1,2,3,4,5};
+    cout << "Array elements: ";
+    for(int i=0;i<5;i++){
+        cout << arr[i] << ", ";
+    }
+    return 0;
+}
 
