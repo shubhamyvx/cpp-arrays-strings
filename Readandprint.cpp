9426314ea34b7a53ac048a4f07af45be8167b23a
Shubham <shubham86.y@gmail.com>
2026-07-15 21:38:10 +0530

    Initial commit

--- Readandprint.cpp
+++ Readandprint.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <array>
+using namespace std;
 
+int main(){
+    array<int,5> arr;
+    cout << "Enter elements:";
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<5;i++){
+        cout << arr[i] << ", ";
+    }
+    return 0;
+}   
