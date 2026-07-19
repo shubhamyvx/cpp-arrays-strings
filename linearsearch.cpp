580f3bb4339bb1dcb154c1f8144c1f5d3f56ef95
Shubham <shubham86.y@gmail.com>
2026-07-19 20:22:04 +0530

    Initial commit

---
+++ linearsearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    bool found = false;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at index " << i;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << "Element not found.";
+
+    return 0;
+}
