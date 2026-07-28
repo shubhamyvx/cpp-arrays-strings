4dc2715bcc6c7fc5eb2f1ff54b89ee26ea9b8d11
Shubham <shubham86.y@gmail.com>
2026-07-28 22:53:34 +0530

    Initial commit

---
+++ findlastoccurnc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char ch;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Enter character to search: ";
+    cin >> ch;
+
+    int index = -1;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ch)
+            index = i;
+    }
+
+    if (index == -1)
+        cout << "Character not found.";
+    else
+        cout << "Last occurrence at index " << index;
+
+    return 0;
+}
